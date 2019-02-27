6a31a900c422ce58fd6256e47f15c2e69ce3dd02
Sean Alexander Clarke <sean.alexander.clarke@gmail.com>
2019-02-27 14:23:06 -0800

    Made snake smarter

--- logic.go
+++ logic.go
@@ -8,8 +8,8 @@ import (
 
 
 var (
-	randomModifier float32 = 1 // range of 0-2 determining entropy's effect on snake's movement
-	hungerModifier float32 = 1 // rating of 0-2 determining hunger's effect on snake's movement
+	randomModifier float32 = 0.1 // range of 0-2 determining entropy's effect on snake's movement
+	hungerModifier float32 = 50 // rating of 0-2 determining hunger's effect on snake's movement
 	fearModifier float32 = 1 // rating of 0-2 determining pessimism's effect on snake's movement
 	directionShift = map[Direction][]int {
 		Up: {-1, 0},
@@ -34,13 +34,14 @@ func (matrix Matrix) printMatrix(repr bool) {
 			for x := range matrix.Matrix[y] {
 				if matrix.Matrix[y][x].Food {
 					fmt.Printf("%s ", "F")
+				} else if matrix.Matrix[y][x].Tenure > 9 {
+					fmt.Printf("%d", matrix.Matrix[y][x].Tenure)
 				} else {
 					fmt.Printf("%d ", matrix.Matrix[y][x].Tenure)
 				}
 			}
 			fmt.Printf("%s", "\n")
 		}
-		fmt.Println("")
 		fmt.Println("")
 		return
 	}
@@ -71,12 +72,28 @@ func (matrix Matrix) printMatrix(repr bool) {
 */
 func (matrix Matrix) populateMatrix(data Req) {
 	// set food
-	for idx := range data.Board.Food {
-		food := &data.Board.Food[idx]
+	for i := range data.Board.Food {
+		food := &data.Board.Food[i]
 		matrix.Matrix[food.Y][food.X].Food = true
 	}
 
 	// set tenure / matrix's heads
+	for i := range data.Board.Snakes {
+		snake := &data.Board.Snakes[i]
+		id := snake.ID
+		head := snake.Body[0]
+		length := len(snake.Body)
+
+		if id != data.You.ID {
+			matrix.Heads = append(matrix.Heads, Position{head.Y, head.X})
+		}
+		matrix.Matrix[head.Y][head.X].Tenure = length - 1
+
+		for p := range snake.Body[1:length] {
+			tail := &snake.Body[p]
+			matrix.Matrix[tail.Y][tail.X].Tenure = length - 1 - p
+		}
+	}
 } 
 
 /*
@@ -133,13 +150,26 @@ func (matrix Matrix) initMatrix(data Req) {
 *		lorem ipsum
 */
 func (matrix Matrix) rateSquare(y int, x int, origin Direction, distance int, depth int) float32 {
+	// return 0 if out of bounds
 	if x == -1 || x == matrix.Width || y == -1 || y == matrix.Height {
 		return 0
 	}
-	if depth == 0 {
-		return matrix.Matrix[y][x].Base
+	// return 0 if occupied square
+	if matrix.Matrix[y][x].Tenure >= distance {
+		return 0
 	}
 
+	base := matrix.Matrix[y][x].Base
+	if matrix.Matrix[y][x].Food {
+		base += float32(100 / (distance * distance)) * hungerModifier
+	}
+
+	// base case
+	if depth == 0 {
+		return base
+	}
+
+	// recursive case
 	var nodes = map[Direction]Direction{
 		Up: Down,
 		Left: Right,
@@ -151,13 +181,14 @@ func (matrix Matrix) rateSquare(y int, x int, origin Direction, distance int, de
 	var rating float32 = 0
 
 	for direction, opposite := range nodes {
-		rating += matrix.Matrix[y][x].Base * matrix.rateSquare(
+		value:= matrix.rateSquare(
 			y + directionShift[direction][0],
 			x + directionShift[direction][1],
 			opposite,
 			distance + 1,
 			depth - 1,
 		)
+		rating += base * value / 3
 	}
 
 	return rating
@@ -183,9 +214,13 @@ func step(data Req) Direction {
     	matrix.Matrix[i] = allocation[i*matrix.Width: (i+1)*matrix.Width]
 	}
 
+	// fmt.Println()
+	// fmt.Printf("%s: ", "turn")
+	// fmt.Printf("%d", data.Turn)
+	// fmt.Println()
 	matrix.initMatrix(data)
-	matrix.printMatrix(false) // print matrix values
-	matrix.printMatrix(true) // print matrix object repr
+	// matrix.printMatrix(false) // print matrix values
+	// matrix.printMatrix(true) // print matrix object repr
 
 	var directions = map[Direction]Direction{
 		Up: Down,
@@ -208,14 +243,23 @@ func step(data Req) Direction {
 	var next Direction
 	var confidence float32 = 0
 
+	if data.You.Health > 75 {
+		hungerModifier = 0
+	} else {
+		hungerModifier = 100
+	}
+
 	for direction, opposite := range directions {
 		var c = matrix.rateSquare(
 			y0 + directionShift[direction][0],
 			x0 + directionShift[direction][1],
 			opposite,
-			1, // maybe should be 0
-			3,
+			1,
+			12,
 		)
+		// fmt.Printf("%s: ", direction)
+		// fmt.Printf("%.3f", c)
+		// fmt.Println()
 		if c > confidence {
 			next = direction
 			confidence = c
